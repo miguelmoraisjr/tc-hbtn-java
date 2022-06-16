d11db51cb81a708f3e3b6c6947ee05e568895429
miguelmoraisjr <miguelj@ciandt.com>
2022-06-16 19:35:06 -0300

    Check

--- 0x00-java-hello_world/PrintStringVariable.java
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -1,6 +1,6 @@
 public class PrintStringVariable {
     public static void main(String[] args) {
         String texto = "Holberton School";
-        System.out.print(texto + "\n" + texto + "\n" + texto.substring(0,9) + "\n");
+        System.out.print(texto + "\n" + texto + "\n" + texto.substring(0,9));
     }
 }

ed348df4134a76f2e12d9b7f4c7edddbd3f145b6
miguelmoraisjr <miguelj@ciandt.com>
2022-06-17 10:23:05 -0300

    Ajustando pontos

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,9 +1,12 @@
 import java.text.Format;
+import java.util.Locale;
 
 public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
+        final var localeBrasil = new Locale("pt", "BR");
+        Locale.setDefault(localeBrasil);
         System.out.printf("Valor: R$ %,.2f\n", valor);
         System.out.printf("Taxa: %.2f%%\n", taxa);
     }

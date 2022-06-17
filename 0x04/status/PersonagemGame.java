69fceb32912262634b08f8bd0a51047f645bb07f
miguelmoraisjr <miguelj@ciandt.com>
2022-06-17 16:22:12 -0300

    Status

---
+++ 0x04/status/PersonagemGame.java
@@ -0,0 +1,47 @@
+public class PersonagemGame {
+
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+
+    public void setSaudeAtual(int saudeAtual) {
+        this.saudeAtual = saudeAtual;
+        if (this.saudeAtual > 0) {
+            this.status = "vivo";
+        } else {
+            this.status = "morto";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        this.saudeAtual -= quantidadeDeDano;
+        setSaudeAtual(saudeAtual);
+        if (saudeAtual <= 0){
+            setSaudeAtual(0);
+        }
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        this.saudeAtual += quantidadeDeCura;
+        setSaudeAtual(saudeAtual);
+        if (saudeAtual >= 100) {
+            setSaudeAtual(100);
+        }
+    }
+}

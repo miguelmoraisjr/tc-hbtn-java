bdf1bb31293698e3031f9c27f8f025e8c0295e56
miguelmoraisjr <miguelj@ciandt.com>
2022-06-21 15:31:48 -0300

    Retangulo Completo

---
+++ 0x05/retangulo_completo/FormaGeometrica.java
@@ -0,0 +1,9 @@
+public class FormaGeometrica {
+
+    public FormaGeometrica(){
+    }
+    public double area() {
+        throw new UnsupportedOperationException("Funcao area nao implementada");
+    }
+
+}

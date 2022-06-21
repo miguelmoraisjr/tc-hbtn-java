da78b3c01f28a5147c85f0c176f54bbe324bfac7
miguelmoraisjr <miguelj@ciandt.com>
2022-06-21 11:31:34 -0300

    Numeros pares

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,10 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        for (int i=0; i < 98; i++){
+            if (i % 2 == 0){
+                System.out.print(i + ", ");
+            }
+        }
+        System.out.println("98");
+    }
+}

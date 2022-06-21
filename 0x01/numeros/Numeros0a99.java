b57f6a9944884cb0b1807d5b8cbbd264c9c797c1
miguelmoraisjr <miguelj@ciandt.com>
2022-06-21 11:24:44 -0300

    Task01 estruturas de repeticao

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,9 @@
+public class Numeros0a99 {
+    public static void main(String[] args) {
+
+        for (int i=0; i < 99; i++){
+            System.out.print(i + ", ");
+        }
+        System.out.println("99");
+    }
+}

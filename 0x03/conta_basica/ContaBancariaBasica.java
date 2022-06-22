c8ee36d5d5eb58973689482a9e6bc3420ded459b
miguelmoraisjr <miguelj@ciandt.com>
2022-06-22 11:38:14 -0300

    Conta Bancaria

---
+++ 0x03/conta_basica/ContaBancariaBasica.java
@@ -0,0 +1,64 @@
+import exceptions.OperacaoInvalidaException;
+
+public class ContaBancariaBasica {
+
+    private String numeracao;
+    private double saldo;
+    private double jurosAnual;
+
+    public ContaBancariaBasica(String numeracao, double jurosAnual) {
+        this.numeracao = numeracao;
+        this.saldo = 0.0;
+        this.jurosAnual = jurosAnual;
+    }
+
+    public String getNumeracao() {
+        return numeracao;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public double getJurosAnual() {
+        return jurosAnual;
+    }
+
+    public void depositar(double valor) throws OperacaoInvalidaException {
+        if (valor <= 0 ) {
+            throw new OperacaoInvalidaException("Valor para deposito deve ser maior que 0");
+        } else {
+            this.saldo += valor;
+        }
+    }
+
+    public void sacar(double valor) throws OperacaoInvalidaException {
+        if (valor <= 0 ) {
+            throw new OperacaoInvalidaException("Valor de saque deve ser maior que 0");
+        } else  if (saldo < valor){
+            throw new OperacaoInvalidaException("Valor de saque deve ser menor que o saldo atual");
+        } else {
+            this.saldo -= valor;
+        }
+    }
+
+    public double calcularTarifaMensal() {
+        if ((saldo * 10 / 100) > 10.00) {
+           return 10.00;
+        } else {
+          return (saldo * 10 / 100);
+        }
+    }
+
+    public double calcularJurosMensal() {
+        if (saldo < 0 ) {
+          return 0.0;
+        } else {
+           return saldo * (jurosAnual / 12 / 100);
+        }
+    }
+
+    public void aplicarAtualizacaoMensal() {
+        this.saldo = saldo + calcularJurosMensal() - calcularTarifaMensal();
+    }
+}

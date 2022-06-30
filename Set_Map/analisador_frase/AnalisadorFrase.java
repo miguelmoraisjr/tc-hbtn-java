a5344ee3d944ce603281d84629c13825d5a99f0f
miguelmoraisjr <miguelj@ciandt.com>
2022-06-30 11:06:10 -0300

    Ajustes

--- Set_Map/analisador_frase/AnalisadorFrase.java
+++ Set_Map/analisador_frase/AnalisadorFrase.java
@@ -5,48 +5,6 @@ import java.util.TreeMap;
 
 public class AnalisadorFrase {
 
-    public static TreeMap contagePalavras(String texto){
-        TreeMap<String, Integer> listMap = new TreeMap<>();
-        List<String> list = contar(texto);
-        int contagem = 1;
-        for (String palavra : list) {
-            String nova = menusculo(palavra);
-            listMap.put(nova, contagem);
-
-        }
-        return listMap;
-    }
-
-    public static List contar(String texto){
-        List<String> palavras = new ArrayList<>();
-        List<String> alteradas = new ArrayList<>();
-        String[] fields = texto.split(" ");
-        for (int i=0; i < fields.length; i++){
-            palavras.add(fields[i]);
-        }
-        for (String palavra : palavras) {
-            alteradas.add(trocarPalavras(palavra));
-        }
-        return alteradas;
-    }
-
-    public static String trocarPalavras(String texto) {
-        String novoTexto = texto;
-        if (texto.contains("?")) {
-            novoTexto = texto.replace("?", "");
-        } else if (texto.contains("!")) {
-            novoTexto = texto.replace("!", "");
-        } else if (texto.contains(".")) {
-            novoTexto = texto.replace(".", "");
-        }
-        return novoTexto;
-    }
-
-    public static String menusculo(String texto){
-        return texto.toLowerCase();
-    }
-
-
     public static TreeMap<String, Integer> contagemPalavras( String texto){
         String[] palavras = texto.toLowerCase(Locale.ROOT).split("[?.! ]+");
         TreeMap<String, Integer> lista = new TreeMap<String, Integer>();

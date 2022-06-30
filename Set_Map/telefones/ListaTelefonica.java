96eb9f37b18e54d2a216e02bad69391c82e30557
miguelmoraisjr <miguelj@ciandt.com>
2022-06-30 11:50:02 -0300

    Lista Telefonica

--- Set_Map/telefones/ListaTelefonica.java
+++ Set_Map/telefones/ListaTelefonica.java
@@ -9,5 +9,21 @@ public class ListaTelefonica {
         list = new HashMap<>();
     }
 
+    public void adicionarTelefone(String nome, Telefone telefone) {
+        ArrayList<Telefone> lista = buscar(nome);
+        if (lista == null) {
+            ArrayList<Telefone> newList = new ArrayList<Telefone>();
+            newList.add(telefone);
+            list.put(nome, newList);
+        } else {
+            lista.add(telefone);
+            list.put(nome, lista);
+        }
+    }
+
+    public ArrayList<Telefone> buscar(String nome){
+        return list.get(nome);
+    }
+
 
 }

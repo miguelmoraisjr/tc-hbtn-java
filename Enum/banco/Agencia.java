1875d3ca0524d44ba23cdcd481c48d7bca9f5ad4
miguelmoraisjr <miguelj@ciandt.com>
2022-07-01 23:19:52 -0300

    Banco

---
+++ Enum/banco/Agencia.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class Agencia {
+
+    private String nome;
+    private ArrayList<Cliente> clientes;
+
+    public Agencia(String nome) {
+        this.nome = nome;
+        this.clientes = new ArrayList<Cliente>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public ArrayList<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public boolean novoCliente(String nomeDoCliente, double transacaoInicial) {
+        //Optional<Cliente> cliente = Optional.ofNullable(buscarCliente(nomeDoCliente));
+        Cliente cliente = buscarCliente(nomeDoCliente);
+        if (cliente == null) {
+            clientes.add(new Cliente(nomeDoCliente, transacaoInicial));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean adicionarTransacaoCliente(String nomeDoCliente, double valorTransacao) {
+        Cliente cliente = buscarCliente(nomeDoCliente);
+        if (cliente != null) {
+            cliente.adicionarTransacao(valorTransacao);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Cliente buscarCliente(String nomeDoCliente) {
+        for (Cliente cliente : clientes) {
+            if (cliente.getNome().equals(nomeDoCliente)) {
+                return cliente;
+            }
+        }
+        return null;
+    }
+}
---
+++ Enum/banco/Banco.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+
+public class Banco {
+
+    private String nome;
+    private ArrayList<Agencia> agencias;
+
+    public Banco(String nome) {
+        this.nome = nome;
+        this.agencias = new ArrayList<Agencia>();
+    }
+
+    public Agencia buscarAgencia(String nomeDaAgencia) {
+        for (Agencia agencia : agencias) {
+            if (agencia.getNome().equals(nomeDaAgencia)) {
+                return agencia;
+            }
+        }
+        return null;
+    }
+
+    public boolean adicionarAgencia(String nomeDaAgencia) {
+        Agencia agencia = buscarAgencia(nomeDaAgencia);
+        if (agencia == null) {
+            agencias.add(new Agencia(nomeDaAgencia));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean adicionarCliente(String nomeDaAgencia, String nomeDoCliente, double transacaoInicial) {
+        Agencia agencia = buscarAgencia(nomeDaAgencia);
+        Cliente cliente = agencia.buscarCliente(nomeDoCliente);
+        if (cliente == null) {
+            agencia.novoCliente(nomeDoCliente, transacaoInicial);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean adicionarTransacaoCliente(String nomeDaAgencia, String nomeDoCliente, double valorTransacao) {
+        Agencia agencia = buscarAgencia(nomeDaAgencia);
+        if (agencia == null) {
+            return false;
+        } else {
+            Cliente cliente = agencia.buscarCliente(nomeDoCliente);
+            if (cliente == null) {
+                return false;
+            } else {
+                cliente.adicionarTransacao(valorTransacao);
+                return true;
+            }
+        }
+    }
+
+    public boolean listarClientes(String nomeDaAgencia, boolean imprimiTransacoes) {
+        Agencia agencia = buscarAgencia(nomeDaAgencia);
+        if (imprimiTransacoes == false) {
+            for (int i=1; i <= agencia.getClientes().size(); i++) {
+                System.out.println("Cliente: " + agencia.getClientes().get(i).getNome() + " [" + i + "]");
+            }
+            return false;
+        } else {
+            for (int i=0; i <= agencia.getClientes().size() -1; i++) {
+                System.out.println("Cliente: " + agencia.getClientes().get(i).getNome() + " [" + (i+1) + "]");
+                int cont = 1;
+                for (Double transacao : agencia.getClientes().get(i).getTransacoes()) {
+                    System.out.printf("[%d] valor %.2f\n", cont, transacao);
+                }
+            }
+            return true;
+        }
+    }
+}

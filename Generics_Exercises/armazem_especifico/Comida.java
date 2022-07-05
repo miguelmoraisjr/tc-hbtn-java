0e6c53df82cddb82641350c16ba8524d4470c4c6
miguelmoraisjr <miguelj@ciandt.com>
2022-07-05 15:28:22 -0300

    Armazem Especifico

---
+++ Generics_Exercises/armazem_especifico/Comida.java
@@ -0,0 +1,29 @@
+public class Comida {
+
+    private String nome;
+    private double calorias;
+    private double preco;
+
+    public Comida(String nome, int calorias, double preco) {
+        this.nome = nome;
+        this.calorias = calorias;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getCalorias() {
+        return calorias;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %.6f R$ %.6f", nome, calorias, preco);
+    }
+}

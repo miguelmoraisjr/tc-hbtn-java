3fa2f7f453787eca8798b7bdb442503049e69a52
miguelmoraisjr <miguelj@ciandt.com>
2022-07-11 12:24:43 -0300

    Set Locale

--- Lambda_Exercicios/predicado/Program.java
+++ Lambda_Exercicios/predicado/Program.java
@@ -5,6 +5,8 @@ import java.util.function.*;
 
 public class Program {
     public static void main(String[] args) {
+        final var localeBrasil = new Locale("pt", "BR");
+        Locale.setDefault(localeBrasil);
         Produto produto1 = new Produto("Cafe Chocolate Trufado Baggio",
                 21.99, 250, 120, TiposProduto.COZINHA);
 
@@ -50,4 +52,4 @@ public class Program {
         System.out.println(produtosCozinha);
         System.out.println(produtosBaixoEstoque);
     }
-}    
+}

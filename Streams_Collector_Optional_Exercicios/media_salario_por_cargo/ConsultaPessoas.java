d2256968d09b1976192e98dded778fbd133c5c08
miguelmoraisjr <miguelj@ciandt.com>
2022-07-12 16:18:32 -0300

    Agrupar por cargo e media salarial

---
+++ Streams_Collector_Optional_Exercicios/media_salario_por_cargo/ConsultaPessoas.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas) {
+        Map<String, Long> list = pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+        return list;
+    }
+
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> pessoas) {
+        Map<String, Map<Integer, Long>> list = pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.groupingBy(Pessoa::getIdade,Collectors.counting())));
+        return  list;
+    }
+
+    public static Map<String, Double> obterMediaSalarioPorCargo(List<Pessoa> pessoas) {
+        Map<String, Double> list = pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.averagingDouble(Pessoa::getSalario)));
+        return list;
+    }
+}

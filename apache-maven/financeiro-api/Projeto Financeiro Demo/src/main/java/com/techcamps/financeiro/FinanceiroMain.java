5f4ac23b50f26b5459d72eca248f993eabb5d370
miguelmoraisjr <miguelj@ciandt.com>
2022-07-14 14:21:30 -0300

    Maven

--- apache-maven/financeiro-api/Projeto Financeiro Demo/src/main/java/com/techcamps/financeiro/FinanceiroMain.java
+++
@@ -1,7 +0,0 @@
-package com.techcamps.financeiro;
-
-public class FinanceiroMain {
-    public static void main(String[] args) {
-        System.out.println("Criando um projeto Java Maven.");
-    }
-}
